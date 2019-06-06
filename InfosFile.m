605af7e4f5433e941fe039ba4b423f324326faa8
Simon <cabanes.simon@gmail.com>
2019-06-06 09:27:20 +0200

    ff

---
+++ InfosFile.m
@@ -0,0 +1,10 @@
+Nri=60; % number of points in r
+Nti=360; % number of points in theta
+Nrk=60; % number of modes in radius
+
+R=29.7; % radius of the tank in cm
+
+Cz = 0.5;
+Ck = 6;
+beta = 0.53; % cm-1.s-1
+epsilon = 2.1*10.^(-4); % cm-2.s-3
