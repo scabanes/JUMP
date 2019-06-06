95f71badab12907ec14b667737dab0a243875ff9
Simon <cabanes.simon@gmail.com>
2019-06-06 13:03:12 +0200

    dd

--- InfosFile.m
+++ InfosFile.m
@@ -13,6 +13,22 @@ Nti=360; % number of points in theta
 Nrk=60; % number of modes in radius
 
 %---------------------------------------------
+%-------------- Truncation parameters
+%-- on azimuth
+%Ntmin=1; Ntmax=Nti;   % Keep full azimuthal domain
+Ntmin=221; Ntmax=310;  % Narrow the domain in azimuth, !! for m interger
+%-- on radius
+Nrmin=1; Nrmax=Nri;    % Keep full radial domain 
+%Nrmin=10; Nrmax=40;   % Narrow the domain in radius
+
+%---------------------------------------------
+%-------------- Frames wanted
+Tmax=nan % if nan inputs the default value will be the number of frames 
+        % contain in the input velocity file.
+Tmax=10;
+Itime=1;
+
+%---------------------------------------------
 %-------------- Experimental device dimensions
 R=29.7; % radius of the tank in cm
 
@@ -22,3 +38,8 @@ Cz = 0.2;
 Ck = 6;
 beta = 0.53; % cm-1.s-1
 epsilon = 2.1*10.^(-4); % cm-2.s-3
+
+%---------------------------------------------
+%-------------- Options
+windowing = 0;  % if 0 no windowing is performed
+ifsave = 1;     % if 0 no saving is performed
