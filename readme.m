32704a8f9210fd02cf4a773acc19f55c001e6e13
Simon <cabanes.simon@gmail.com>
2019-06-06 14:20:53 +0200

    ee

--- readme.m
+++ readme.m
@@ -1,5 +1,40 @@
-% Step by step with script.
-%__________________________________________________________________________
+% Step by step with JUMP.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% _________________________________________________________________________
+%                               VASCA
+% _________________________________________________________________________
+% A) --- Spectral analysis
+%% MAIN:
+%->>> SpectralAnalysis_FB_Galperin2015.m
+% Here we compute the spectral analysis. Everything is in cm !!
+%% Infos:
+%->>> InfosFile.m
+% This file has to be nest in the repository where fes data of the velocity
+% fields are.
+%% Sub-routines:
+%->>> FourierBesselDecomp.m
+% Function that compute the spectral decomposition.
+%->>> Maps_Galperin2015.m
+% This maps the first time step to see the sector you are working on.
+%->>> Windowing.m
+% This is a function that creates matrices for signal windowing in a
+% choosen direction and with Tukey or Hanning functions.
+%->>> loadmtx.m
+% This load the .mtx files of the velocity fields.
+%% Plot-routines:
+%->>> SpectralAnalysis_Plots.m
+% Plots files saved in the Main.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% _________________________________________________________________________
+%                               TORINO
+% _________________________________________________________________________
 % 1) --- Data load & infos
 % ## Load data from the Turnbase:
 % https://turbase.cineca.it/init/routes/#/logging/view_dataset/99/tabmeta
@@ -31,10 +66,4 @@
 % of the points to take out on the radius when there is too few data to
 % have a complete radial profil.
 %                           --> L_M is the result of PV mono.
-% _________________________________________________________________________
-% 6) --- Spectral analysis
-% ## Windowing.m:
-% This is a function that creates matrices for signal windowing in a
-% choosen direction and with Tukey or Hanning functions.
-% ## FourierBessel_Trunc_Time_windowing_Torino.m
-% Here we compute the spectral analysis. Everything is in cm !!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
