a1cca4224754c36c5c6852bb3c8b1dec304a1a0c
Simon <cabanes.simon@gmail.com>
2020-01-17 16:44:24 +0100

    ee

--- Main_Torino.m
+++ Main_Torino.m
@@ -17,7 +17,7 @@ load('besselzeros2_C.mat');
 load([roots,Name,'/infos.mat'])
 %---------------------------------------------
 %-------------- Frames & Tocm
-nFrames = ((endi-starti)/step + 1);
+nFrames = 1100%((endi-starti)/step + 1);
 Tmax=nFrames;
 Itime=1;
 nTime = Tmax-Itime+1;
@@ -32,18 +32,24 @@ Create_Grid_pol_Torino_cm
 % #########################################################################
 %                                                                      RUN:
 % #########################################################################
+% 
 %--------------------------------------------------------------------------
 % Waring!! The grid vector r and theta are modified when running the
 % programm SpectralAnalysis_FB. Spectral Analysis and PV Monotonization
 % have then to be run separately.
 %--------------------------------------------------------------------------
 
-% ->>> Spectral Analysis
+% ->>> Spectral Analysis in Polar coordinate
 %SpectralAnalysis_FB
 
 %SpectralAnalysis_Plots
 
+% ->>> Spectral Analysis in Cartesian coordinate
+SpectralAnalysis_FF
+
+SpectralAnalysis_FF_Plots
+
 % ->>> PV Monotonization
 %DerivateVorticita
 
-PV_mono
+% PV_mono
